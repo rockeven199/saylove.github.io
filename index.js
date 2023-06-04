49efe143b2235e31735e25b1d3920f43b0910584
rockeven199 <67353571+rockeven199@users.noreply.github.com>
2023-06-04 23:30:20 +0800

    update

--- index.js
+++ index.js
@@ -263,17 +263,50 @@ function lover() {
 
 
 function doingCard() {
+    const start = document.querySelector(".start")
     const cardPart1 = document.querySelector(".part1");
     const cardContainer = document.querySelector(".card");
     const topEmojy = document.querySelector(".cat-pic-1");
-    cardPart1.addEventListener('click', (element) => {
-        let flag = element.target.dataset.canopenflag;
+    const topEmojyImg = document.querySelector(".cat-pic-1 img");
+    const me = document.querySelector("#me");
+    start.addEventListener('click', () => {
+        let flag = cardPart1.dataset.canopenflag;
         if (flag == "true") {
             cardContainer.removeAttribute('style')
             cardContainer.style.transform = "rotate(0deg)";
             cardContainer.style.top = "10%";
             cardPart1.style.animation = "openCard 1s ease-in-out forwards";
             topEmojy.style.opacity = "1";
+            setTimeout(() => {
+                start.style.zIndex = "1";
+            }, 500);
+            cardPart1.dataset.canopenflag = "false";
+        } else {
+            cardContainer.style.transform = "rotate(180deg)";
+            cardContainer.style.top = "25%";
+            start.style.zIndex = "999";
+            cardPart1.style.animation = "closeCard 1s ease-in-out forwards";
+            cardPart1.dataset.canopenflag = "true";
         }
     });
+
+    me.addEventListener('click', () => {
+        let flag = cardPart1.dataset.canopenflag;
+        if (flag == "true") {
+            cardContainer.removeAttribute('style')
+            cardContainer.style.transform = "rotate(0deg)";
+            cardContainer.style.top = "10%";
+            cardPart1.style.animation = "openCard 1s ease-in-out forwards";
+            topEmojy.style.opacity = "1";
+            setTimeout(() => {
+                start.style.zIndex = "99";
+            }, 500);
+            cardPart1.dataset.canopenflag = "false";
+        }
+    });
+}
+
+window.onload = () => {
+    lover();
+    doingCard();
 }
